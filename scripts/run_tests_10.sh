5ddbd43295dde84213faf4af21407382f5576e51
Michael Ostertag <mosterta@ucsd.edu>
2020-03-07 12:43:25 -0800

    Added scripts folder, containing some basic run configs

---
+++ scripts/run_tests_10.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+# Runs simulation tests for opt-trajectory project
+#
+# Source code should be located in opt-trajectory/src
+
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+
+echo "${DIR}"
+
+source "${DIR}/../../env_drone/bin/activate"
+
+list_cond=( "--B 10 --vmax 10 --amax 30 --Ntests 10 --Nq 10" \
+            "--B 10 --vmax 20 --amax 30 --Ntests 10 --Nq 10" \
+            "--B 10 --vmax 30 --amax 30 --Ntests 10 --Nq 10" \
+            "--B 10 --vmax 10 --amax 10 --Ntests 10 --Nq 10" \
+            "--B 10 --vmax 20 --amax 10 --Ntests 10 --Nq 10" \
+            "--B 10 --vmax 30 --amax 10 --Ntests 10 --Nq 10" )
+
+for config in "${list_cond[@]}"
+do
+    echo "${config}"
+    echo "python3 ${DIR}/../src/main_sim.py ${config}"
+    python3 "${DIR}/../src/main_sim.py" ${config}
+done
