93a7d41f6cb60a08e79381fed8b8f4c35c50c713
Michael Ostertag <mosterta@ucsd.edu>
2021-01-19 13:42:38 -0800

    drone_models: Updated Phantom models with better PID values, created new Phantom model for velocity controller

--- src/drone_models.py
+++ src/drone_models.py
@@ -66,8 +66,43 @@ class Phantom3:
         self.maxF = 2.20 * m * g
         self.minF = 0.05 * m * g
 
-        self.k_pi = np.array([2, 2, 0.8])
-        self.k_di = np.array([2.2, 2.2, 0.5])
+        self.k_pi = np.array([1, 1, 3])
+        self.k_di = np.array([2, 2, 3])
 
         self.k_p = np.array([250, 250, 250])
         self.k_d = np.array([20, 20, 20])
+
+        self.maxXYaccel = 2 * self.maxF / self.mass * np.sin(self.maxangle)
+
+class Phantom3_vel:
+    def __init__(self):
+        """
+        Parameters for the Phantom3 and a velocity-based plan
+
+        Model assumptions from Phantom3 description
+        """
+
+        m = 1.216
+        g = 9.81
+        I = np.array([[0.03356,       0,       0],
+                      [      0, 0.03356,       0],
+                      [      0,       0, 0.06782]]) # Scaled version of Crazyflie inertial tensor
+        L = 0.350  # arm length in m
+
+        self.mass = m
+        self.I = I
+        self.invI = np.linalg.inv(I)
+        self.grav = g
+        self.arm_length = L
+
+        self.maxangle = 60 * np.pi / 180
+        self.maxF = 2.20 * m * g
+        self.minF = 0.05 * m * g
+
+        self.k_pi = np.array([1.1, 1.1, 3.3])
+        self.k_di = np.array([3, 3, 4.5])
+
+        self.k_p = np.array([250, 250, 250])
+        self.k_d = np.array([20, 20, 20])
+
+        self.maxXYaccel = 1.0 * self.maxF / self.mass * np.sin(self.maxangle)
